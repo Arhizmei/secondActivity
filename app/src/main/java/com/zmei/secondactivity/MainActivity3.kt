e0488ea5160ced59e2ff5c43f6f529b7c432a287
Arhizmei <arhizmei@gmail.com>
2023-03-01 12:31:53 +0200

    secondActivity

---
+++ app/src/main/java/com/zmei/secondactivity/MainActivity3.kt
@@ -0,0 +1,24 @@
+package com.zmei.secondactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+    fun onClickMain (view: View) {
+
+    val intent = Intent(this, MainActivity::class.java)
+    startActivity(intent)
+    }
+
+    fun onClickSecond (view : View) {
+
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+}

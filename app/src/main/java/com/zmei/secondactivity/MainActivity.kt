2d631f433399c680d7b60664595711417404b17b
arhizmei <arhizmei@gmail.com>
2023-03-05 21:11:41 +0200

    secondData

--- app/src/main/java/com/zmei/secondactivity/MainActivity.kt
+++ app/src/main/java/com/zmei/secondactivity/MainActivity.kt
@@ -4,21 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.zmei.secondactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
     }
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == 100 && resultCode== RESULT_OK && data != null){
+                bindingClass.textView1.text = data.getStringExtra("key2")
+
+            }
+        }
 
 
-    fun onClickSecond (view : View) {
-
-        val intent = Intent(this, SecondActivity::class.java)
-        startActivity(intent)
+    fun onClick (view: View){
+        val i = Intent(this, SecondActivity::class.java)
+        i.putExtra("key", "Proverka")
+        startActivityForResult(i, 100)
     }
-    fun onClickClose (view : View) {
-        finish()}
-
 
 }
--- app/src/main/java/com/zmei/secondactivity/SecondActivity.kt
+++ app/src/main/java/com/zmei/secondactivity/SecondActivity.kt
@@ -5,23 +5,26 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.zmei.secondactivity.databinding.LayoutSecondBinding
 
 class SecondActivity : AppCompatActivity() {
 
+    lateinit var bindingClass : LayoutSecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        bindingClass = LayoutSecondBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_second)
+        setContentView(bindingClass.root)
+
+        val message = intent.getStringExtra("key")
+        bindingClass.textView2.text = message
     }
 
 
-    fun onClickMai3 (view : View) {
 
-        val intent = Intent(this, MainActivity3::class.java)
-        startActivity(intent)
-    }
-    fun onClickClose (view : View) {
+    fun onClickBack (view: View){
 
+        intent.putExtra("key2", bindingClass.edText2.text.toString())
+        setResult(RESULT_OK, intent)
         finish()
     }
-
 }
